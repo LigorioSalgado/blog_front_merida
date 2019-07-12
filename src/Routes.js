dd38a4d5802afff0dd3312eece3ed309a71d3a65
Edwin Salgado <edwin@devf.mx>
2019-07-11 19:57:53 -0500

    HOC auth

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import isAuthenticated from './utils/IsAuthenticated';
 import Home from './views/Home';
 import Post from './views/Post';
 import Login from './views/Login';
 import Signup from './views/Signup';
 
+
 function Logout(){
 	localStorage.removeItem('blogToken')
 	return <Redirect to="/login" />
 }
+
+const SecureLogout = isAuthenticated(Logout);
 
 function Routes() {
   return (
@@ -17,7 +21,7 @@ function Routes() {
 		<Route exact path="/post/:id" component={Post} />
 		<Route exact path="/login" component={Login} />
 		<Route exact path="/signup" component={Signup} />
-		<Route exact path="/logout" component={Logout} />
+		<Route exact path="/logout" component={SecureLogout} />
 
 
    </>
